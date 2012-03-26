68b8b6af64c9d287bba9341e644bef0f0dc2f48b
Sridhar Sundaram <sridhar.sundaram@gmail.com>
2012-03-26 08:41:03 +0530

    Some more simplification - lesson1data handler was not required.

--- simple/main.py
+++ simple/main.py
@@ -3,14 +3,12 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from handlers.register_handler import RegisterHandler
 from handlers.learner_handler import LearnerHandler
 from handlers.home_handler import HomeHandler
-from handlers.lesson1data import Lesson1Data
 
 application = webapp.WSGIApplication(
                                      [
                                       (r'/([0-9]{10})', HomeHandler),
                                       ('/', RegisterHandler),
                                       ('/learner', LearnerHandler),
-                                      ('/lesson1.data', Lesson1Data),
                                     ],
                                      debug=True)
 
